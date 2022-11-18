a9b9b8d5989b6bcaa0cabc87f81e6e80b5d72933
Vinokur <vinokur.n@gmail.com>
2022-11-17 20:31:31 -0500

    Add project files.

---
+++ AcumaticaTest/TestBase/FunctionAcumatica.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace AcumaticaTest
+{
+    public class FunctionAcumatica
+    {
+        static void Main()
+        {
+        }
+
+        /// <summary>
+        /// Main required function returns TRUE of FALSE based on the following requirements.
+        /// If C= 0, then possible values of A and B are: А >= 14, В >= 14
+        /// If C> 0, then possible values of А and В are: А > 2*(C+1),В > 2*(C+1)
+        /// </summary>
+        /// <param name="c"></param>
+        /// <param name="b"></param>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public bool Function(int c, int b, int a)
+        {
+            Console.WriteLine($"c = {c}, a = {a}, b = {b} ");
+
+            if (c == 0)
+            {
+                if (a >= 14 && b >= 14)
+                {
+                    Console.WriteLine($"When int c = {c} then a and b must be >=14, what is true");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine($"When int c = {c} then a and b must be >=14, what is false");
+                    return false;
+                }
+            }
+            else if (c > 0)
+            {
+                int temp = 2 * (c + 1);
+                if (a > temp && b > temp)
+                {
+                    Console.WriteLine($"When int c > 0 and equal {c}, a and b must be more than {temp}, what is true");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine($"When int c > 0 and equal {c}, a and b must be more than {temp}, what is false");
+                    return false;
+                }
+            }
+            else Console.WriteLine($" Int c = {c}, which is < 0, not right condition");
+            return false;
+        }
+
+    }
+}
---
+++ AcumaticaTest/Tests/Tests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+
+namespace AcumaticaTest
+{
+    [TestFixture]
+    [Category("AcumaticaTests")]
+    public class Tests : FunctionAcumatica
+    {
+        #region PositiveTests
+
+        [Test]
+        [Description("Positive tests c=>0")]
+        [Category("Positive")]
+        // C = 0
+        [TestCase(0, 14, 14, ExpectedResult = true)]
+        [TestCase(0, 15, 15, ExpectedResult = true)]
+        [TestCase(0, 14, 236, ExpectedResult = true)]
+        [TestCase(0, 100500, 14, ExpectedResult = true)]
+        [TestCase(0, 2147483647, 2147483647, ExpectedResult = true)]
+        // c > 0
+        [TestCase(2, 7, 7, ExpectedResult = true)]
+        [TestCase(2, 8, 300, ExpectedResult = true)]
+        public bool TestsPositive(int c, int b, int a)
+        {
+            bool result = Function(c, b, a);
+            return result;
+        }
+
+        #endregion PositiveTests
+
+        #region NegativeTests
+
+        [Test]
+        [Category("Negative")]
+        // c = 0
+        [TestCase(0, 14, 13, ExpectedResult = false)]
+        [TestCase(0, 1, 14, ExpectedResult = false)]
+        [TestCase(0, 16, -100, ExpectedResult = false)]
+        [TestCase(0, -3, 14, ExpectedResult = false)]
+        // c > 0
+        [TestCase(2, 6, 6, ExpectedResult = false)]
+        [TestCase(2, 6, 7, ExpectedResult = false)]
+        [TestCase(2, 7, 6, ExpectedResult = false)]
+        // c < 0
+        [TestCase(-1, 6, 6, ExpectedResult = false)]
+        public bool NegativeTests(int c, int b, int a)
+        {
+            bool result = Function(c, b, a);
+            return result;
+        }
+
+        #endregion NegativeTests
+    }
+}
+
